017af2f7575e3f47ca5f3771460985119fdce286
Victor Cionca <victorcionca@gmail.com>
2019-04-24 07:35:42 +0100

    n-bit gray code generator

--- graycode.py
+++ graycode.py
@@ -1,3 +1,4 @@
+import numpy as np
 decimal_to_gray = {
         0:0,
         1:1,
@@ -45,3 +46,24 @@ def decode(symbol):
         out = out ^ mask
         mask = mask >> 1
     return out
+
+def generate_n_bit_gray(n):
+    """
+    Generate an n-bit Gray code.
+    Starts with 1 bit code and generates
+    recursively.
+    """
+    crt_code = np.array([[0],[1]])
+    crt_bits = 1
+    while crt_bits < n:
+        # Reflect the current one
+        new_code = np.flip(crt_code, 0)
+        # Prefix old code with 0
+        old_code = np.concatenate((np.zeros((len(crt_code),1)), crt_code), axis=1)
+        # Prefix new code with 1
+        new_code = np.concatenate((np.ones((len(new_code),1)), new_code), axis=1)
+        # Concatenate old with new
+        crt_code = np.concatenate((old_code, new_code), axis=0)
+        crt_bits += 1
+    return crt_code
+        
